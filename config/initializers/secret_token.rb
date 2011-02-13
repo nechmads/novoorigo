61891e1a5abea887373af79c638681626f7c2763
shahar <me@shaharnechmad.com>
2011-02-13 10:23:19 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Novoorigo::Application.config.secret_token = 'afc148eee0cd4aef3ec917802ff741f089c8769fa21314442d39d8fbe905323c791f3e038d5f82922a198084637748e5f41b56e50f8f7cae7927cd18b09476af'
